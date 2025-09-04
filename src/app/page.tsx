0065d26a3dedb204d9e5789644152dde26663c37
drulias <matthew@drulias.com>
2025-09-04 15:45:42 -0700

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { db } from "~/server/db";
 import Link from "next/link";
 
 const mockUrls = [
@@ -14,10 +15,17 @@ const mockImages = mockUrls.map((url, index) => ({
     url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+
+    const posts = await db.query.posts.findMany();
+    console.log(posts)
+
   return (
     <main className="">
             <div className="flex flex-wrap gap-4">
+                {posts.map((post) => (
+                    <div key={post.id}>{post.name}</div>
+                ))}
                 {[...mockImages, ...mockImages, ...mockImages].map((image) => (
                     <div key={image.id} className="w-48">
                         <img src={image.url} />
