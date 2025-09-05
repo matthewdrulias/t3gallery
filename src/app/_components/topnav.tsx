0f597cabd4abe8a0421c49f78f92928d5ab5d170
drulias <matthew@drulias.com>
2025-09-05 15:03:03 -0700

    Added Authentication

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export function TopNav() {
+    return (
+    <nav className="flex items-center justify-between w-full border p-4 text-xl font-semibold">
+            <div>Gallery</div>
+            <div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+    </nav>
+    )
+}
