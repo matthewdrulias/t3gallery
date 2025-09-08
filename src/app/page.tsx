ec9c6d7999a599ee8b0956c31813e6fe786d85c1
drulias <matthew@drulias.com>
2025-09-07 21:10:53 -0700

    Use Next Js Image...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@ import { db } from "~/server/db";
 import Link from "next/link";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
+import Image from 'next/image';
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
     const images = await getMyImages();
 return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
         {images.map((image) => (
-            <div key={image.id} className="w-48 flex-col">
-                <img src={image.url} />
+            <div key={image.id} className="flex h-48 w-48 flex-col">
+                <Image
+                        src={image.url}
+                        style={{objectFit: "contain"}}
+                        width={192}
+                        height={192}
+                        alt={image.name} />
                 <div>{image.name}</div>
             </div>
         ))}
