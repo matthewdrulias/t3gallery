b4dbd4b5e0603e5c8d0f8ba2d4de450b5562e9d4
drulias <matthew@drulias.com>
2025-09-28 13:53:21 -0700

    custom upload button & add ShadCN

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,24 +1,16 @@
-'use client'
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "~/utils/uploadthing";
-import { useRouter } from "next/navigation";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
-    const router = useRouter();
     return (
-    <nav className="flex items-center justify-between w-full border p-4 text-xl font-semibold">
+    <nav className="flex items-center justify-between w-full border-b p-4 text-xl font-semibold">
             <div>Gallery</div>
-            <div className="flex flex-row">
+            <div className="flex flex-row gap-4">
                 <SignedOut>
                     <SignInButton />
                 </SignedOut>
                 <SignedIn>
-                    <UploadButton
-                        endpoint="imageUploader"
-                        onClientUploadComplete={() => {
-                            router.refresh();
-                        }}
-                />
+                    <SimpleUploadButton />
                     <UserButton />
                 </SignedIn>
             </div>
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -15,11 +15,11 @@ export default async function FullPageImageView(props: { id: number }) {
             <div className="flex w-48 flex-col flex-shrink-0 border-l ">
                 <div className="text-lg border-b text-center p-2">{image.name}</div>
                 <div className="flex flex-col p-2">
-                    <span>Uploaded by</span>
+                    <span>Uploaded By:</span>
                     <span>{uploaderInfo.fullName}</span>
                 </div>
                 <div className="flex flex-col p-2">
-                    <span>Created On</span>
+                    <span>Created On:</span>
                     <span>{new Date(image.createdAt).toLocaleDateString()}</span>
                 </div>
             </div>
